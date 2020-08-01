9b541c2dd18d41cebd16cb2fc9973dd23295d8f4
Tushar Kumar <tushar.kumar.999@gmail.com>
2020-07-31 19:34:21 -0500

    Implemented delete functionality.

--- src/model/persistence/CommandDeleteShape.java
+++ src/model/persistence/CommandDeleteShape.java
@@ -1,33 +1,33 @@
-//package model.persistence;
-//
-//import model.interfaces.ICommand;
-//import view.interfaces.PaintCanvasBase;
-//
-//public class CommandDeleteShape implements ICommand {
-//
-//    private final ShapeList shapeList;
-//    private final PaintCanvasBase paintCanvas;
-//
-//    public CommandDeleteShape(ShapeList shapeList, PaintCanvasBase paintCanvas){
-//        this.shapeList = shapeList;
-//        this.paintCanvas = paintCanvas;
-//    }
-//
-//    @Override
-//    public void run() {
-//        System.out.println("Delete Command");
-//
-//        for (Shape shape : shapeList.getSelectShapeList()) {
-//            shapeList.addDeleteShape(shape);
-//            shapeList.removeDrawShape(shape);
-//        }
-//
-//        ClearCanvas.clear(paintCanvas);
-//
-//        for (Shape shape : shapeList.getDrawShapeList()) {
-//            FactorySelectShape factorySelectShape = new FactorySelectShape();
-//            factorySelectShape.select(paintCanvas, shape);
-//        }
-//        System.out.println("# of Shapes Deleted " + shapeList.getDeleteShapeList().size());
-//    }
-//}
+package model.persistence;
+
+import model.interfaces.ICommand;
+import view.interfaces.PaintCanvasBase;
+
+public class CommandDeleteShape implements ICommand {
+
+    private final ShapeList shapeList;
+    private final PaintCanvasBase paintCanvas;
+
+    public CommandDeleteShape(ShapeList shapeList, PaintCanvasBase paintCanvas){
+        this.shapeList = shapeList;
+        this.paintCanvas = paintCanvas;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Delete Command");
+
+        for (Shape shape : shapeList.getSelectShapeList()) {
+            shapeList.addDeleteShape(shape);
+            shapeList.removeDrawShape(shape);
+        }
+
+        ClearCanvas.clear(paintCanvas);
+
+        for (Shape shape : shapeList.getDrawShapeList()) {
+            FactorySelectShape factorySelectShape = new FactorySelectShape();
+            factorySelectShape.select(paintCanvas, shape);
+        }
+        System.out.println("# of Shapes Deleted " + shapeList.getDeleteShapeList().size());
+    }
+}
